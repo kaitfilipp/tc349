a23614eb3157d19ce7d6257814c3447c1f3bc21a
kaitfilipp <filippka@msu.edu>
2013-02-05 20:21:19 -0500

    personal site work

---
+++ projects/personal-site/js/personal-site.js
@@ -0,0 +1,15 @@
+function hello() {
+	alert("My favorite animal is a giraffe.");
+}
+
+function design() {
+	alert("My favorite color is red.");
+}
+
+function experience() {
+	alert("I've never had a pet. :(");
+}
+
+function photography() {
+	alert("Harry Potter is my favorite book series.");
+}
